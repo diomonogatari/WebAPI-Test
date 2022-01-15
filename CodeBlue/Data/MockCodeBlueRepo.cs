f04e706ff99fb5b538e8a0aafc5713fd9341f74c
Diomonogatari <diodude.lol@gmail.com>
2022-01-15 18:21:17 +0000

    missing documentation

--- CodeBlue/Data/MockCodeBlueRepo.cs
+++ CodeBlue/Data/MockCodeBlueRepo.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// <copyright file="MockCodeBlueRepo.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace CodeBlue.Data
 {
-    public class MockCodeBlueRepo
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using CodeBlue.Models;
+
+    /// <summary>
+    /// The mock code blue repo.
+    /// </summary>
+    public class MockCodeBlueRepo : ICodeBlueRepo
     {
+        /// <inheritdoc/>
+        public Customer GetCustomerByName(string firstname, string surname)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
