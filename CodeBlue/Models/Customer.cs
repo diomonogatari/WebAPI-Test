d3f2e2c66f7be32138bae5ccaeea5290833d1e29
Diomonogatari <diodude.lol@gmail.com>
2022-01-15 17:40:01 +0000

    Base Model

---
+++ CodeBlue/Models/Customer.cs
@@ -0,0 +1,32 @@
+// <copyright file="Customer.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CodeBlue.Models
+{
+    /// <summary>
+    /// The customer.
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the surname.
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address.
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
