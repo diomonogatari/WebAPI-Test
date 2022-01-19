314580b53706d81520ed5dde53f3bbbbfe0f50e5
Diomonogatari <diodude.lol@gmail.com>
2022-01-19 17:06:10 +0000

    Initial migration

---
+++ CodeBlue/Migrations/20220119164031_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿namespace CodeBlue.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <summary>
+    /// The initial migration.
+    /// This will create the Customer's table that will as specified on the test requirements.
+    /// </summary>
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc/>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 128, nullable: false),
+                    Surname = table.Column<string>(maxLength: 128, nullable: false),
+                    EmailAddress = table.Column<string>(maxLength: 256, nullable: false),
+                    Password = table.Column<string>(maxLength: 1024, nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc/>
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
--- CodeBlue/Models/Customer.cs
+++ CodeBlue/Models/Customer.cs
@@ -4,6 +4,8 @@
 
 namespace CodeBlue.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     /// <summary>
     /// The customer.
     /// </summary>
@@ -17,21 +19,29 @@ namespace CodeBlue.Models
         /// <summary>
         /// Gets or sets the first name.
         /// </summary>
+        [Required]
+        [MaxLength(128)]
         public string FirstName { get; set; }
 
         /// <summary>
         /// Gets or sets the surname.
         /// </summary>
+        [Required]
+        [MaxLength(128)]
         public string Surname { get; set; }
 
         /// <summary>
         /// Gets or sets the email address.
         /// </summary>
+        [Required]
+        [MaxLength(256)]
         public string EmailAddress { get; set; }
 
         /// <summary>
         /// Gets or sets the password.
         /// </summary>
+        [Required]
+        [MaxLength(1024)]
         public string Password { get; set; }
     }
 }
